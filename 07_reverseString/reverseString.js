a547d18fce8fc1df8c5e8d50d4c073c69726208f
Om Upadhyay <88921660+Upadhyay-Om@users.noreply.github.com.>
2025-08-21 01:35:06 +0530

    Solution for 07_reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    return str.split('').reverse().join(''); 
 };
+const result = reverseString("hello there");
+console.log(result)
 
 // Do not edit below this line
 module.exports = reverseString;
