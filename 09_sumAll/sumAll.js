45063e001265bc991d8a62e5dd2f0a7ba217088e
Om Upadhyay <omupadhyay2007@gmail.com>
2025-08-21 03:39:48 +0530

    Adding comments

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,20 +1,12 @@
 const sumAll = function(min,max) {
-    if (!Number.isInteger(min)||!Number.isInteger(max)) return "ERROR";
-    if (min <0 || max < 0) return "ERROR";
+    if (!Number.isInteger(min)||!Number.isInteger(max)) return "ERROR"; // For integers 
+    if (min <0 || max < 0) return "ERROR"; // For Negative nos 
     if (min > max) {
         const temp = min;
         min = max;
         max = temp;
     }
-    
 
-  
-  
-  
-  
-  
-  
-  
   let sum = 0;
   for (let i = min; i <= max; i++) {
     sum += i;
